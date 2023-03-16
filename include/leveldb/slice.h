f40c334015f50e4a4d2a3e3b430bbd7f36e24fa9
Ap0l1o <44611118+Ap0l1o@users.noreply.github.com>
2023-03-16 13:30:00 +0800

    Update slice.h
    Should the function compare (const Slice& b) call b.data () and b.size () instead of using b.data_ and b.size_

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -98,8 +98,8 @@ inline bool operator==(const Slice& x, const Slice& y) {
 inline bool operator!=(const Slice& x, const Slice& y) { return !(x == y); }
 
 inline int Slice::compare(const Slice& b) const {
-  const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
-  int r = memcmp(data_, b.data_, min_len);
+  const size_t min_len = (size_ < b.size() ? size_ : b.size_;
+  int r = memcmp(data_, b.data(), min_len);
   if (r == 0) {
     if (size_ < b.size_)
       r = -1;
